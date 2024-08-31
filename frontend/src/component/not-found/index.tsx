2a51f83a7ca357c16253ddc61ff629d8444f40d1
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-09-01 03:16:04 +0530

    feat: added not found page

---
+++ frontend/src/component/not-found/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl">404</h1>
+      <p className="text-xl">Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
