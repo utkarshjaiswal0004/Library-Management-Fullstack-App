c0baf0295bdd709e98b22d9f97d6f7455b82a579
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-08-31 21:23:56 +0530

    test: added vitest for unitesting

---
+++ frontend/vitest.setup.ts
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach, expect } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
