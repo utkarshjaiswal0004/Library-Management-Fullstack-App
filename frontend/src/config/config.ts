bc81f608d21e9635c98f85b40a5ba70f72f231fd
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-09-04 03:51:45 +0530

    chore: vercel deploy test for frontend with VITE env  url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,4 @@
 // const API_URL = import.meta.env.REACT_APP_API_URL;
-const API_URL = "https://library-management-backend-cl18.onrender.com/api/";
+const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 export default API_URL;
