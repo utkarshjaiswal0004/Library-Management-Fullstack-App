3a0b703318dbc66d9a55797fd1680ed0b6b8828f
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-09-04 03:42:26 +0530

    chore: vercel deploy test for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/library-management-fullstack-app",
   test: {
     globals: true,
     environment: "jsdom",
